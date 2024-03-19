bf0894dad03fe2cbf2ae839dfa9fcf3e2399f4d3
Prateek Koirala <prateekkoirala77@gmail.com>
2024-03-19 11:41:05 +0545

    add bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_management_system/screen/chats.dart';
-import 'package:food_management_system/screen/my_cart.dart';
-import 'package:food_management_system/screen/payment_success.dart';
-import 'package:food_management_system/screen/payment_unsuccess.dart';
-import 'screen/log_in.dart';
-import 'screen/welcome_screen.dart';
+import 'package:food_management_system/screen/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Food Management System',
       // home: PaymentUsuccess(),
